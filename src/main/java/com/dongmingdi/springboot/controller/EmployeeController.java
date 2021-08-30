639f9259f5b55aae478218610460cdc774b5a154
dongmingdi <176963928@qq.com>
2021-08-30 15:13:54 +0800

    基础版

--- src/main/java/com/dongmingdi/springboot/controller/EmployeeController.java
+++ src/main/java/com/dongmingdi/springboot/controller/EmployeeController.java
@@ -1,22 +1,63 @@
 package com.dongmingdi.springboot.controller;
 
+import com.dongmingdi.springboot.dao.DepartmentDao;
 import com.dongmingdi.springboot.dao.EmployeeDao;
+import com.dongmingdi.springboot.entities.Department;
 import com.dongmingdi.springboot.entities.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
 @Controller
 public class EmployeeController {
 
-    EmployeeDao employeeDao = new EmployeeDao();
+    @Autowired
+    EmployeeDao employeeDao;
+
+    @Autowired
+    DepartmentDao departmentDao;
 
     @GetMapping("/emps")
     public String list(Model model) {
         Collection<Employee> employees = employeeDao.getAll();
         model.addAttribute("emps", employees);
         return "emp/list";
+    }
+
+    @GetMapping("/emp")
+    public String toAddPage(Model model) {
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts", departments);
+        return "emp/add";
+    }
+
+    @PostMapping("/emp")
+    public String addEmp(Employee employee) {
+        employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+
+    @GetMapping("/emp/{id}")
+    public String toEditPage(@PathVariable("id") Integer id, Model model) {
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("emp", employee);
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts", departments);
+        return "emp/add";
+    }
+
+    @PutMapping("/emp")
+    public String updateEmp(Employee employee) {
+        employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+
+    @DeleteMapping("/emp/{id}")
+    public String delEmp(@PathVariable("id") Integer id) {
+        employeeDao.delete(id);
+        return "redirect:/emps";
     }
 }
