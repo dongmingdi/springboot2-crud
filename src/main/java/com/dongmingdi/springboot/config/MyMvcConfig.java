ff927db476d3c1068112dd8caa4fb4f1afd302c6
dongmingdi <176963928@qq.com>
2021-08-30 17:37:12 +0800

    WebServerFactoryCustomizer

--- src/main/java/com/dongmingdi/springboot/config/MyMvcConfig.java
+++ src/main/java/com/dongmingdi/springboot/config/MyMvcConfig.java
@@ -2,6 +2,8 @@ package com.dongmingdi.springboot.config;
 
 import com.dongmingdi.springboot.component.LoginHandlerInterceptor;
 import com.dongmingdi.springboot.component.MyLocalResolver;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
@@ -12,10 +14,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MyMvcConfig implements WebMvcConfigurer {
 
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer() {
+        return new WebServerFactoryCustomizer<ConfigurableWebServerFactory>() {
+            @Override
+            public void customize(ConfigurableWebServerFactory factory) {
+                factory.setPort(8088);
+            }
+        };
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-//        WebMvcConfigurer.super.addInterceptors(registry);
-        registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**").excludePathPatterns("/index.html","/","/user/login","/**/asserts/**");
+        WebMvcConfigurer.super.addInterceptors(registry);
+//        registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**").excludePathPatterns("/index.html","/","/user/login","/**/asserts/**");
     }
 
     @Override
