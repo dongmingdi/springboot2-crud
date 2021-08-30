308dff7dd755d0c5fdddd51498e79b2ecfde6531
dongmingdi <176963928@qq.com>
2021-08-30 22:08:38 +0800

    注册三大组件

--- src/main/java/com/dongmingdi/springboot/config/MyServerConfig.java
+++ src/main/java/com/dongmingdi/springboot/config/MyServerConfig.java
@@ -1,14 +1,34 @@
 package com.dongmingdi.springboot.config;
 
+import com.dongmingdi.springboot.filter.MyFilter;
+import com.dongmingdi.springboot.listener.MyListener;
 import com.dongmingdi.springboot.servlet.MyServlet;
 import org.springframework.boot.web.server.ConfigurableWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Arrays;
+
 @Configuration
 public class MyServerConfig {
+
+    @Bean
+    public ServletListenerRegistrationBean myListener() {
+        ServletListenerRegistrationBean<MyListener> servletListenerRegistrationBean = new ServletListenerRegistrationBean(new MyListener());
+        return servletListenerRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean myFilter() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new MyFilter());
+        filterRegistrationBean.setUrlPatterns(Arrays.asList("/hello", "/myServlet"));
+        return filterRegistrationBean;
+    }
 
     @Bean
     public ServletRegistrationBean myServlet() {
